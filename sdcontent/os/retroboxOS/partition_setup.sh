f2ae285072f5e03d617db79b9b3851adcee9903e
digitalLumberjack <digitallumberjack@gmail.com>
2015-01-17 16:14:10 +0100

    v2.7.0

--- sdcontent/os/retroboxOS/partition_setup.sh
+++ sdcontent/os/retroboxOS/partition_setup.sh
@@ -14,7 +14,7 @@ mount "$part2" /tmp/2
 
 sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part2}|"
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
-sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
+sed /tmp/2/etc/fstab -i -e "s|^.*/boot|${part1} /boot|"
 sed /tmp/2/etc/fstab -i -e "s|^.*/retrobox/share|${part3} /retrobox/share|"
 
 umount /tmp/1
